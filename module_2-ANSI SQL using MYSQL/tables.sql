b9542bae6dfbe2370e1b3ebbca9bb1636e290f36
saisaketh_709 <sakethchenna056@gmail.com>
2026-05-29 14:54:57 +0530

    created tables

--- module_2-ANSI SQL using MYSQL/tables.sql
+++ module_2-ANSI SQL using MYSQL/tables.sql
@@ -70,9 +70,7 @@ CREATE TABLE resources (
 
 INSERT INTO events (event_id, title, description, city, start_date, end_date, status, organizer_id) VALUES
 (1, 'Tech Innovators Meetup', 'A meetup for tech enthusiasts.', 'New York', '2025-06-10 10:00:00', '2025-06-10 16:00:00', 'upcoming', 1),
-
 (2, 'AI & ML Conference', 'Conference on AI and ML advancements.', 'Chicago', '2025-05-15 09:00:00', '2025-05-15 17:00:00', 'completed', 3),
-
 (3, 'Frontend Development Bootcamp', 'Hands-on training on frontend tech.', 'Los Angeles', '2025-07-01 10:00:00', '2025-07-03 16:00:00', 'upcoming', 2);
 
 INSERT INTO sessions (session_id, event_id, title, speaker_name, start_time, end_time) VALUES
